0bd6f3188a9842d8c1a8a06b72ff482688e497e6
MIND <79649658+manu-neg@users.noreply.github.com>
2025-12-05 19:56:35 -0400

    comments

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from core.implementations.DefaultAlgorithm import DefaultAlgorithm
 
 
 def main():
+    """
+    Docstring for main
+
+    Los decoradores mejoran las capacidades de extender e integrar distintas funcionalidades
+    en las implementaciones de los algoritmos sin modificar su codigo fuente.
+
+    En este ejemplo, se decora DefaultAlgorithm con Logger y Recorder.
+    1. Logger registra las llamadas y los datos yieldados por DefaultAlgorithm.
+    2. Recorder procesa los datos yieldados (simulando grabacion o almacenamiento).
+
+    Para mejorar la trazabilidad de los datos se puede tambien definir un tipo de dato
+    para cada funcionalidad decoradora para identificar el origen de los datos y filtrar
+    segun sea necesario.
+
+    """
     proc = Recorder(Logger(DefaultAlgorithm()))
     # `Logger.execute` (and the decorated `execute` methods) are generators
     # (they use `yield`). Calling them without iterating will not run
