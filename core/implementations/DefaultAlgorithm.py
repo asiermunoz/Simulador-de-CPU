61cc7b0b0350e4a971df4b841347a88837a51921
MIND <79649658+manu-neg@users.noreply.github.com>
2025-12-17 11:19:35 -0400

    documentacion

--- core/implementations/DefaultAlgorithm.py
+++ core/implementations/DefaultAlgorithm.py
@@ -37,4 +37,31 @@ Puedes decorar cualquier implementacion de Algorithm con Logger para obtener log
 
 todas las clases son basadas en Executor
 
+Cambios nuevos:
+
+    1. Implementacion de parameter object concreta (Ver Log y Timestamp)
+    2. Decoracion por capas (Ver estructura en main.py)
+    3. Monitoreo constante (Ver decoradores)
+
+Como funciona:
+
+    La funcionalidad de generadores de python ayuda a mejorar esta implementacion
+    ya que los generadores "Lazy" son iterables, por lo que podemos iterar infinitamente
+    y obtener resultados en tiempo real sin utilizar un codigo complejo.
+
+    Para implementar esto sin los generadores, necesitariamos un elemento concreto a traves
+    el cual pueden pedir datos los distintos decoradores del tipo callback o una pila.
+
+Como podemos utilizar esto para el proyecto:
+
+    Si nuestro DefaultAlgorithm es un algoritmo de planificacion, podemos consumir los
+    datos iniciales hasta que la pila de procesos se acabe. Ya que tenemos esta funcionalidad
+    unidireccional, podemos implementar N capas donde cada capa tenga un proposito, y cada capa
+    deje "pasar" valores destinados a otras capas.
+
+    De esta forma, el algoritmo no debe tener mucha complicacion, solo retornar elementos especificos
+    a conveniencia, y dada una buena implementacion de las capas, el resto del trabajo como mediciones
+    y logs se haran automaticamente. Si queremos un componente grafico, podriamos progamar una capa 
+    con esa responsabilidad, etc.
+
 """
