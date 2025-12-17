85cf3459507ac8968a141c1ffb7a3c2e8403d1c4
MIND <79649658+manu-neg@users.noreply.github.com>
2025-12-17 17:35:21 -0400

    auto update pip

--- manage.py
+++ manage.py
@@ -3,8 +3,17 @@
 import os
 import sys
 
+EXPORT_DEPTENCIES = True
+
+def export_dependencies(file_path: str):
+    """Export the current dependencies to a requirements.txt file."""
+    print(f"Exporting dependencies to {file_path}...")
+    os.system(f'pip freeze > {file_path}')
+
 
 def main():
+    if EXPORT_DEPTENCIES:
+        export_dependencies('requirements.txt')
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
@@ -15,6 +24,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
